46a3a6791a186bb74cf18b3624091f52c32e3d8d
romanegarou <romanegallier@gmail.com>
2017-04-05 20:07:04 +0200

    m

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
-cc cgiu.c proc.c testproc.c -o testproc
-cc rdjpeg.c read_image.c -o read_image
-cc rdjpeg.c proc.c creation_histo.c -o creation_histo
+creation_histo: rdjpeg.c proc.c creation_histo.c 
+	cc rdjpeg.c proc.c creation_histo.c -o creation_histo
+
+read_image: rdjpeg.c read_image.c
+	cc rdjpeg.c read_image.c -o read_image
+
+testproc: cgiu.c proc.c testproc.c
+	cc cgiu.c proc.c testproc.c -o testproc
+clean: 
+	rm creation_histo *.o
+
+
--- creation_histo.c
+++ creation_histo.c
@@ -1,6 +1,3 @@
-
-
-
 /* regarder la valeur pour le rouge le bleu et le vert 
 diviser par 64
 multiplier le rouge par 4
@@ -14,19 +11,19 @@ on normalise ensuite tab
 * 
 * 
 */
-#include "proc.h"
+
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
 #include <math.h>
-
+#include "proc.h"
 #include "rdjpeg.h"
 
 
 //il faut mettre un nom de fileNameReq
-3/0;
+//3/0;
 #define fileNameReq "imageReq.png"
 #define pathToUrls "urls.txt"
 
@@ -39,12 +36,12 @@ KEY images[N];
 
 
 
-void distance_euclidienne(char *fileName1, char *fileName2) {
-  float h1[64];
-  float h2[64];
+double distance_euclidienne(char *fileName1, char *fileName2){
+	float h1[64];
+	float h2[64];
 
-  FILE* f1 = fopen(fileName1, 'r');
-  FILE* f2 = fopen(fileName2, 'r');
+  FILE* f1 = fopen(fileName1, "r");
+  FILE* f2 = fopen(fileName2, "r");
 
   fread(h1, sizeof(float), 64, f1);
   fread(h2, sizeof(float), 64, f2);
@@ -57,37 +54,34 @@ void distance_euclidienne(char *fileName1, char *fileName2) {
 
 //  res = sqrtf(res);  //pas besoin pour le tri
 
-  printf("%d\n",res);
+  printf("%f\n",res);
+  return res;
 }
-  
 
 //set element i of the KEY array images[N]. sets the value to distance(fileReq, fileTargeted)
 void setElementi(int i, char * fileTargeted) {
 //  printf("setting image %i...\n",i);
-  images[i]->k = i;
-  images[i]->d = distance_euclidienne(fileNameReq, fileTargeted);
+  images[i].k = i;
+  images[i].d = distance_euclidienne(fileNameReq, fileTargeted);
 }
 
 
 void sort() {
-  qsort(images, N, sizeof(KEY), KeyCompare);
+  qsort(images, N, sizeof(KEY), keyCompare);
 }
-
-
-
 
 int main(int argc, char *argv[])
 {
 
-	int i,j,nx,ny,num;
+	int i,j,nx,ny,num,x;
 
 	CIMAGE cim;
 	float histogramme[64];
 
 	FILE* FO= fopen ("histo.txt", "w");
 	int n;
-   char** url =readList("urls.txt",&n);
-   for (i=0; i<n;i++){
+    char** url =readList("urls.txt",&n);
+    for (i=0; i<n;i++){
 			//calcul de l'histogramme de ce fichier 
 			
 			/*------------------------------------------------*/
@@ -97,8 +91,8 @@ int main(int argc, char *argv[])
 			read_cimage(url[i],&cim);
 			printf(" je suis la\n");
 			/*initialisation de l'histogramme*/
-			for (i=0;i<64;i++){
-				histogramme[i]=0.0;
+			for (x=0;x<64;x++){
+				histogramme[x]=0.0;
 			}
 
 			printf("Largeur de l'image : %d\n",cim.nx);
@@ -107,17 +101,20 @@ int main(int argc, char *argv[])
 			nx=cim.nx;
 			ny=cim.ny;
 
-			for (j = 0; j < nx; j++) {       /* ligne par ligne */
-			for (i = 0; i < ny; i++) {   /* pixel par pixel */
-				printf("%4d",cim.r[i][j]);
-				num=cim.r[i][j]/64+(cim.g[i][j]/64)*4+(cim.b[i][j]/64)*16;
-				histogramme[num]++;
+			for (x = 0; x < nx; x++) {       /* ligne par ligne */
+				for (j = 0; j < ny; j++) {   /* pixel par pixel */
+					//printf("%4d",cim.r[x][j]);
+					num=cim.r[x][j]/64+(cim.g[x][j]/64)*4+(cim.b[x][j]/64)*16;
+					histogramme[num]++;
+				}
+				printf (".");
 			}
-			}
+			
+			printf ("je fini de generer l'histo\n");
 
 			//normalisation
-			for (i=0;i<64;i++){
-				histogramme[i]= histogramme[i]/(nx*ny);
+			for (x=0;x<64;x++){
+				histogramme[x]= histogramme[x]/(nx*ny);
 			}
 			/*------------------------------------------------*/
 
