3fcfd2c18a4c4d219c60b15f1bb6b08839f60a9f
Polytrope <alexandre78ferrera@gmail.com>
2017-04-06 17:35:02 +0200

    calcul des distances, tri et affichage des résultats

--- creation_histo.c
+++ creation_histo.c
@@ -30,30 +30,33 @@ on normalise ensuite tab
 
 #define fileNameReq "imageReq.png"
 #define pathToUrls "urls.txt"
+#define fileHisto "histo.txt"
+
 
 //number of pictures
 #define N 9638
 
 
 KEY images[N];
+float histogramme[64]; //l'histogramme du fichier requete
 
 
+//fileName1 : image comparée.
+//h1 : histogramme de l'image recherchée
+void distance_euclidienne(FILE *fileName1) {
+//  float h1[64];
+  float h2[64]; //histogramme de l'image comparée
 
+//  FILE* f1 = fopen(fileName1, 'r');
+//  FILE* f2 = fopen(fileName2, 'r');
 
-void distance_euclidienne(char *fileName1, char *fileName2) {
-  float h1[64];
-  float h2[64];
-
-  FILE* f1 = fopen(fileName1, 'r');
-  FILE* f2 = fopen(fileName2, 'r');
-
-  fread(h1, sizeof(float), 64, f1);
+//  fread(h1, sizeof(float), 64, f1);
   fread(h2, sizeof(float), 64, f2);
 
   float res = 0;
   int i;
   for(i=0 ; i<64 ; i++) {
-    res += (h1[i] - h2[i]) * (h1[i]*h2[i]);
+    res += (histogramme[i] - h2[i]) * (histogramme[i]-h2[i]);
   }
 
 //  res = sqrtf(res);  //pas besoin pour le tri
@@ -62,11 +65,11 @@ void distance_euclidienne(char *fileName1, char *fileName2) {
 }
   
 
-//set element i of the KEY array images[N]. sets the value to distance(fileReq, fileTargeted)
-void setElementi(int i, char * fileTargeted) {
+//set element i of the KEY array images[N]. sets the value to distance(FILE * fileTargeted)
+void setElementi(int i, FILE * fileTargeted) {
 //  printf("setting image %i...\n",i);
   images[i]->k = i;
-  images[i]->d = distance_euclidienne(fileNameReq, fileTargeted);
+  images[i]->d = distance_euclidienne(fileTargeted);
 }
 
 
@@ -86,19 +89,20 @@ int main(int argc, char *argv[])
 	float histogramme[64];
 
 	FILE* FO= fopen ("histo.txt", "w");
+/*
 	int n;
    char** url =readList("urls.txt",&n);
    for (i=0; i<n;i++){
 			//calcul de l'histogramme de ce fichier 
-			
+*/			
 			/*------------------------------------------------*/
 			/* lecture d'une image requête                    */
 			/*------------------------------------------------*/
-			printf ("Procesing %s ...\n",url[i]);
-			read_cimage(url[i],&cim);
-			printf(" je suis la\n");
+//			printf ("Procesing %s ...\n",url[i]);
+//			read_cimage(url[i],&cim);
+//			printf(" je suis la\n");
 			/*initialisation de l'histogramme*/
-			for (i=0;i<64;i++){
+/*			for (i=0;i<64;i++){
 				histogramme[i]=0.0;
 			}
 
@@ -108,27 +112,95 @@ int main(int argc, char *argv[])
 			nx=cim.nx;
 			ny=cim.ny;
 
-			for (j = 0; j < nx; j++) {       /* ligne par ligne */
-			for (i = 0; i < ny; i++) {   /* pixel par pixel */
-				printf("%4d",cim.r[i][j]);
-				num=cim.r[i][j]/64+(cim.g[i][j]/64)*4+(cim.b[i][j]/64)*16;
-				histogramme[num]++;
-			}
+			for (j = 0; j < nx; j++) {       // ligne par ligne 
+  			for (i = 0; i < ny; i++) {   // pixel par pixel 
+	  			printf("%4d",cim.r[i][j]);
+	  			num=cim.r[i][j]/64+(cim.g[i][j]/64)*4+(cim.b[i][j]/64)*16;
+	  			histogramme[num]++;
+	  		}
 			}
 
 			//normalisation
 			for (i=0;i<64;i++){
 				histogramme[i]= histogramme[i]/(nx*ny);
 			}
-			/*------------------------------------------------*/
-
+*/			/*------------------------------------------------*/
+/*
 			fwrite(histogramme, sizeof (float), 64, FO);
 			/*for (i=0;i<64;i++){
 				
 				//printf ("%f ",histogramme[i]);
-			}*/
+			}
 
+	} */
+
+
+//  FILE* freq = fopen(FileNameReq,'r');
+//  fread(histogramme,sizeof(float),64,freq);
+
+	int n;
+  char** url =readList("urls.txt",&n);
+
+  printf("urls lus\n");
+
+	read_cimage(fileNameReq,&cim);
+
+  printf("Image requete lue\n");
+
+  for(i=0; i<64; i++) {
+    histogramme[i]= 0.0;
+  }
+
+	printf("Largeur de l'image : %d\n",cim.nx);
+	printf("Hauteur de l'image : %d\n",cim.ny);
+
+	nx=cim.nx;
+	ny=cim.ny;
+
+	for (j = 0; j < nx; j++) {       // ligne par ligne 
+		for (i = 0; i < ny; i++) {   // pixel par pixel 
+			printf("%4d",cim.r[i][j]);
+			num=cim.r[i][j]/64+(cim.g[i][j]/64)*4+(cim.b[i][j]/64)*16;
+			histogramme[num]++;
+		}
 	}
+
+	//normalisation
+	for (i=0;i<64;i++){
+		histogramme[i]= histogramme[i]/(nx*ny);
+	}
+
+//  int n;
+//  char** url =readList("urls.txt",&n);
+  File * fHistos = fopen(fileHisto,'r');
+
+  for (i=0; i<n;i++){
+    //calcul de l'histogramme de ce fichier 
+
+		/*------------------------------------------------*/
+		/* lecture d'une image requête                    */
+		/*------------------------------------------------*/
+		printf ("Procesing %d/%d ...\n",i,N);
+
+    setElementi(i, fHistos);
+    
+		printf(" je suis la\n");
+	}
+
+  printf("sorting results...\n);
+
+  sort();
+
+  printf("creating res file...\n");
+
+  FILE* Fres = fopen("out.html",'w');
+
+  for(i=0 ; i < n; i++) {
+    fprintf(Fres, "<IMG SRC=\"%s\">\n",url[images[i].k]);
+  }	
+
+
+  fclose(Fres);
 	fclose(FO);
 
   exit(0);
