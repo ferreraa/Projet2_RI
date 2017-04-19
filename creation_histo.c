87a8096fa86266a06ba404b37e7ca36db336daf6
romanegarou <romanegallier@gmail.com>
2017-04-19 11:30:09 +0200

    modif histogramme.c

--- creation_histo.c
+++ creation_histo.c
@@ -18,6 +18,7 @@ on normalise ensuite tab
 #include <string.h>
 #include <strings.h>
 #include <math.h>
+
 #include "proc.h"
 #include "rdjpeg.h"
 
@@ -29,20 +30,16 @@ on normalise ensuite tab
 #define fileHisto "histo.txt"
 
 
-//number of pictures
-#define N 9638
 
-
-KEY images[N];
+int N;
+KEY* images;
 float histogramme[64]; //l'histogramme du fichier requete
 
 
 //fileTargeted : image comparée.
 float distance_euclidienne(FILE *fileTargeted) {
-//  float h1[64];
   float h2[64]; //histogramme de l'image comparée
 
-//  fread(h1, sizeof(float), 64, f1);
   fread(h2, sizeof(float), 64, fileTargeted);
 
   float res = 0;
@@ -50,10 +47,6 @@ float distance_euclidienne(FILE *fileTargeted) {
   for(i=0 ; i<64 ; i++) {
     res += (histogramme[i] - h2[i]) * (histogramme[i]-h2[i]);
   }
-
-//  res = sqrtf(res);  //pas besoin pour le tri
-
-  printf("res = %f\n",res);
   return res;
 }
 
@@ -69,87 +62,48 @@ void sort() {
   qsort(images, N, sizeof(KEY), keyCompare);
 }
 
-int main(int argc, char *argv[])
-{
 
+
+void process_histogramme_sift(float* histogramme,char* fname) {
+	int x;
+	FILE * f = fopen(fname, "r");
+	
+	int num = 0,i=0;
+
+	/*initialisation de l'histogramme*/
+	for (x=0;x<256;x++){
+		histogramme[x]=0.0;
+	}
+
+	while(fscanf(f, "%d",&num)!= EOF) {
+		histogramme[num]++;
+		i++;
+	}
+	//normalisation
+	for (x=0;x<256;x++){
+		histogramme[x]= histogramme[x]/(i);
+	}
+	
+}
+
+
+void creation_histos_sift(){
+	
+}
+
+//créé l'histogramme d'une image donnée	
+void process_histogramme_couleur (float* histogramme, char * url) {
 	int i,j,nx,ny,num,x;
 
 	CIMAGE cim;
-	float histogramme[64];
 
-	FILE* FO= fopen ("histo.txt", "r");
-/*
-	int n;
-    char** url =readList("urls.txt",&n);
-    for (i=0; i<n;i++){
-			//calcul de l'histogramme de ce fichier 
-*/			
-			/*------------------------------------------------*/
-			/* lecture d'une image requête                    */
-			/*------------------------------------------------*/
-//			printf ("Procesing %s ...\n",url[i]);
-//			read_cimage(url[i],&cim);
-//			printf(" je suis la\n");
-			/*initialisation de l'histogramme*/
-/*			for (x=0;x<64;x++){
-				histogramme[x]=0.0;
-			}
+	read_cimage(url,&cim);
 
-			printf("Largeur de l'image : %d\n",cim.nx);
-			printf("Heuteur de l'image : %d\n",cim.ny);
+	printf("Image lue\n");
 
-			nx=cim.nx;
-			ny=cim.ny;
-
-			for (x = 0; x < nx; x++) {       // ligne par ligne 
-				for (j = 0; j < ny; j++) {   // pixel par pixel 
-					//printf("%4d",cim.r[x][j]);
-					num=cim.r[x][j]/64+(cim.g[x][j]/64)*4+(cim.b[x][j]/64)*16;
-					histogramme[num]++;
-				}
-				printf (".");
-			}
-			
-			printf ("je fini de generer l'histo\n");
-
-			//normalisation
-			for (x=0;x<64;x++){
-				histogramme[x]= histogramme[x]/(nx*ny);
-			}
-*/			/*------------------------------------------------*/
-/*
-			fwrite(histogramme, sizeof (float), 64, FO);
-			/*for (i=0;i<64;i++){
-				
-				//printf ("%f ",histogramme[i]);
-			}
-
-	} */
-
-
-//  FILE* freq = fopen(FileNameReq,'r');
-//  fread(histogramme,sizeof(float),64,freq);
-
-  int n;
-  char** url =readList("urls.txt",&n);
-
-  n=10; //TODO RETIRER POUR VISUALISER LE RESULTAT FINAL
-
-  for(i=0; i<n;i++) {
-    printf("url %i : %s\n", i+1, url[i]);
-  }
-
-  printf("urls lus\n");
-
-//-----------------CREATION HISTOGRAMME REQUETE----------
-
-  read_cimage(fileNameReq,&cim);
-
-  printf("Image requete lue\n");
-
-  for(i=0; i<64; i++) {
-    histogramme[i]= 0.0;
-  }
+	for(i=0; i<64; i++) {
+		histogramme[i]= 0.0;
+	}
 
 	printf("Largeur de l'image : %d\n",cim.nx);
 	printf("Hauteur de l'image : %d\n",cim.ny);
@@ -159,7 +113,6 @@ int main(int argc, char *argv[])
 
 	for (x = 0; x < nx; x++) {       // ligne par ligne 
 		for (j = 0; j < ny; j++) {   // pixel par pixel 
-			//printf("%4d",cim.r[x][j]);
 			num=cim.r[x][j]/64+(cim.g[x][j]/64)*4+(cim.b[x][j]/64)*16;
 			histogramme[num]++;
 		}
@@ -170,44 +123,115 @@ int main(int argc, char *argv[])
 	for (i=0;i<64;i++){
 		histogramme[i]= histogramme[i]/(nx*ny);
 	}
+}
 
 
-//------------------------Fin de creation histogramme requete-------
+void creation_histos_couleur() {
 
-
-  FILE * fHistos = fopen(fileHisto,"rb");
-
-  for (i=0; i<n;i++){
-    //calcul de l'histogramme de ce fichier 
-
-		/*------------------------------------------------*/
-		/* lecture d'une image requête                    */
-		/*------------------------------------------------*/
-		printf ("Procesing %d/%d ...\n",i,N);
-
-    setElementi(i, fHistos);
-    
-		printf(" images[%i].k = %i, images[%i].d = %f\n",i,images[i].k,i,images[i].d);
-    
-
+	float histogramme[64];
+	FILE* FO= fopen ("histo.txt", "r");
+	int n,i;
+    char** url =readList("urls.txt",&N);
+    images = malloc(sizeof(KEY)*N);
+    for (i=0; i<n;i++){
+		process_histogramme_couleur (histogramme,url[i]);
+		fwrite(histogramme, sizeof (float), 64, FO);
 	}
-
-  printf("sorting results...\n");
-
-  sort();
-
-  printf("creating res file...\n");
-
-  FILE* Fres = fopen("out.html","w");
-
-  for(i=0 ; i < n; i++) {
-    fprintf(Fres, "<IMG SRC=\"%s\">\n",url[images[i].k]);
-  }	
+}
 
 
-  fclose(Fres);
-  fclose(FO);
+	
+void chercher_image_couleur(char * url, int nb_retour){
+	FILE* FO= fopen ("histo.txt", "r");
+	int i;
+	float histogramme[64]; 
+	process_histogramme_couleur(histogramme,url);
 
-  exit(0);
+	FILE * fHistos = fopen(fileHisto,"rb");
+
+	//TODO trouver une solution pour le parcours de ce fichier 
+	int n=100;
+	for (i=0; i<n;i++){
+   		printf ("Procesing %d/%d ...\n",i,N);
+		setElementi(i, fHistos);
+		printf(" images[%i].k = %i, images[%i].d = %f\n",i,images[i].k,i,images[i].d);
+    }
+
+	printf("sorting results...\n");
+
+	sort();
+
+	printf("creating res file...\n");
+
+	FILE* Fres = fopen("out.html","w");
+
+	for(i=0 ; i < nb_retour; i++) {
+		fprintf(Fres, "<IMG SRC=\"%i\">\n",url[images[i].k]);
+	}	
+
+
+	fclose(Fres);
+	fclose(FO);
+}	
+
+
+void chercher_image_sift(char * url, int nb_retour){
+		FILE* FO= fopen ("histo_sift.txt", "r");
+int i;
+	float histogramme[256]; 
+	process_histogramme_sift(histogramme,url);
+
+	FILE * fHistos = fopen(fileHisto,"rb");
+	for (i=0; i<n;i++){
+   		printf ("Procesing %d/%d ...\n",i,N);
+		setElementi(i, fHistos);
+		printf(" images[%i].k = %i, images[%i].d = %f\n",i,images[i].k,i,images[i].d);
+    }
+
+	printf("sorting results...\n");
+
+	sort();
+
+	printf("creating res file...\n");
+
+	FILE* Fres = fopen("out.html","w");
+
+	for(i=0 ; i < nb_retour; i++) {
+		fprintf(Fres, "<IMG SRC=\"%i\">\n",url[images[i].k]); //TODO trouver l'url de l'image n° images[i].k
+	}	
+
+
+	fclose(Fres);
+	fclose(FO);
+}
+
+void chercher_image_combined(){
+}
+
+int main(int argc, char *argv[])
+{
+	int nb_retour;
+	char url[300];
+	//generer l'histogramme des images en fonction de la couleur :
+	creation_histos_couleur();
+	//generer l'histogramme des images en fonction du clustering : 
+	creation_histos_sift();
+	//chercher une image en fonction des couleurs :
+	printf ("entrez une url \n");
+	
+	scanf("%s", url);
+	printf("entrez un nombre de retour\n");
+	scanf("%i",&nb_retour);
+	chercher_image_couleur(url, nb_retour);
+	//chercher une image en fonction des cluster :
+	printf ("entrez une url \n");
+	scanf("%s", url);
+	printf("entrez un nombre de retour\n");
+	
+	scanf("%i",&nb_retour);
+	chercher_image_sift(url, nb_retour);
+	//cherche une image en fonction des deux :
+	chercher_image_combined();
+	exit(0);
 }
 
